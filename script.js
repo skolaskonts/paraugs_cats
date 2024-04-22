2d855e47c25b5a6ed444e22b48eb227d28a92dac
skolaskonts <marks.lismanis@limbazugimnazija.lv>
2024-04-22 14:11:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert(1);
+//console.log(1)
+function sutitZinu()
+{
+    console.log('sutitZinu()darbojas');
+    let zina= document.querySelector('zina');
+    let zinas= document.querySelector('CataZina');
+    zinas.innerHTML = zinas.innerHTML+zina.value
+}
